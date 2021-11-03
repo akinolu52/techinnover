3afd73eeb40a1e9afc4de555cae2008f1b957899
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2021-11-03 21:02:29 +0100

    init

---
+++ App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet, useColorScheme,
+  View
+} from 'react-native';
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={{ flex: 1 }}>
+
+        <View style={{ flex: 1 }}>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+export { default as AppContainer } from './AppContainer';
+// export { default as Avatar } from './Avatar';
+// export { default as Button } from './Button';
+// export { default as QuickAction } from './cards/QuickAction';
+// export { default as Check } from './Check';
+// export { default as FastImage } from './Image';
+// export { default as FormInput } from './inputs/FormInput';
+// export { default as OtpInput } from './inputs/OtpInput';
+// export { default as PasswordInput } from './inputs/PasswordInput';
+// export { default as PhoneInput } from './inputs/PhoneInput';
+// export { default as PickerInput } from './inputs/PickerInput';
+// export { default as PinInput } from './inputs/PinInput';
+export { default as Text } from './Text';
+
